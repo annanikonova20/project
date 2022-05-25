ee4e9ad71d899b2703d90ac32d8075aec731ec61
annanikonova20 <106188979+annanikonova20@users.noreply.github.com>
2022-05-26 00:25:31 +0300

    1
    1

--- ConsoleApplication1/ConsoleApplication1.cpp
+++ ConsoleApplication1/ConsoleApplication1.cpp
@@ -8,77 +8,87 @@
 #include "stdlib.h"
 using namespace std;
 
-
+void coding(string code[256], int num) //assign code
+{
+	for (int i = 0; i < num - 2; i++)
+	{
+		code[i] = code[i] + '0';
+		for (int j = i + 1; j < num; j++)
+			code[j] = code[j] + '1';
+	}
+	code[num - 2] = code[num - 2] + '0';
+	code[num - 1] = code[num - 1] + '1';
+}
 int main()
 {
-	string S;
+	string s;
 	const int max = 256;
 	int num = 0;
-	cout << "Enter a string" << endl;
-	getline(cin, S);
-	char Sym[257];
-	int Freq[257];
-	char MeetSym[257];
-	int MeetFreq[257];
+	cout << "Enter a string:" << endl;
+	getline(cin, s);
+	char sym[257];
+	int freq[257];
+	char meetsym[257];
+	int meetfreq[257];
+
+	for (int i = 0; i < max;i++) //filling arrays of symbols and frequencies
+		sym[i] = (char)i;
 	for (int i = 0; i < max;i++)
-		Sym[i] = (char)i;
-	for (int i = 0; i < max;i++)
-		Freq[i] = 0;
-	for (int i = 0;i < S.size();i++)
+		freq[i] = 0;
+
+	for (int i = 0;i < s.size();i++) //count the frequency for each element
 		for (int j = 0;j < max;j++)
 		{
-			if (S[i] == Sym[j])
+			if (s[i] == sym[j])
 			{
-				Freq[j]++;
+				freq[j]++;
 				break;
 			}
 		}
-	for (int i = 0;i < max;i++)
+	for (int i = 0;i < max;i++)//determine which symbols are met
 	{
-		if (Freq[i] > 0)
+		if (freq[i] > 0)
 		{
-			MeetSym[num] = Sym[i];
-			MeetFreq[num] = Freq[i];
+			meetsym[num] = sym[i];
+			meetfreq[num] = freq[i];
 			num++;
 		}
 	}
-	for (int i = num - 1;i >= 0;i--)
+//
+	for (int i = num - 1;i >= 0;i--) //sort symbols in order of increasing frequency
 		for (int j = 0;j < i;j++)
 		{
-			if (MeetFreq[j] < MeetFreq[j + 1])
+			if (meetfreq[j] < meetfreq[j + 1])
 			{
-				int Old = MeetFreq[j];
-				MeetFreq[j] = MeetFreq[j + 1];
-				MeetFreq[j + 1] = Old;
+				int Old = meetfreq[j];
+				meetfreq[j] = meetfreq[j + 1];
+				meetfreq[j + 1] = Old;
 
-				Old = (int)MeetSym[j];
-
-				MeetSym[j] = MeetSym[j + 1];
-				MeetSym[j + 1] = (char)Old;
+				Old = (int)meetsym[j];
+				meetsym[j] = meetsym[j + 1];
+				meetsym[j + 1] = (char)Old;
 			}
 		}
-	for (int i = 0;i < num;i++)
-		cout << MeetSym[i] << " " << MeetFreq[i] << endl;
 
 	string node[256];
 	for (int i = 0; i < num; i++)
 	{
-		node[i] = MeetSym[i];
+		node[i] = meetsym[i];
 	}
 	for (int i = num - 1; i > 1; i--)
 	{
-		MeetFreq[i - 1] = MeetFreq[i - 1] + MeetFreq[i];
+		meetfreq[i - 1] = meetfreq[i - 1] + meetfreq[i]; //forming a Huffman tree
 		node[i - 1] = node[i - 1] + node[i];
 		if (i != 2)
 		{
 			for (int k = num - 1;k >= 0;k--)
 				for (int j = 0;j < i;j++)
-				{
-					if (MeetFreq[j] < MeetFreq[j + 1])
+				{//
+					if (meetfreq[j] < meetfreq[j + 1])
 					{
-						int Old = MeetFreq[j];
-						MeetFreq[j] = MeetFreq[j + 1];
-						MeetFreq[j + 1] = Old;
+						int Old = meetfreq[j];
+						meetfreq[j] = meetfreq[j + 1];
+						meetfreq[j + 1] = Old;
 
 						string Old1 = (string)node[j];
 						node[j] = node[j + 1];
@@ -88,50 +98,32 @@ int main()
 		}
 	}
 
-	string code0[256];
+	string code0[256]; //assign code
 	int num0 = node[0].size();
 	for (int i = 0; i < num0; i++)
 		code0[i] = '0';
 	if (num0 != 1)
-	{
-		for (int i = 0; i < num0 - 2; i++)
-		{
-			code0[i] = code0[i] + '0';
-			for (int j = i + 1; j < num0; j++)
-				code0[j] = code0[j] + '1';
-		}
-		code0[num0 - 2] = code0[num0 - 2] + '0';
-		code0[num0 - 1] = code0[num0 - 1] + '1';
-	}
-
+		coding(code0, num0);
 
 	string code1[256];
 	int num1 = node[1].size();
 	for (int i = 0; i < num1; i++)
 		code1[i] = '1';
 	if (num1 != 1)
-	{
-		for (int i = 0; i < num1 - 2; i++)
-		{
-			code1[i] = code1[i] + '0';
-			for (int j = i + 1; j < num1; j++)
-				code1[j] = code1[j] + '1';
-		}
-		code1[num1 - 2] = code1[num1 - 2] + '0';
-		code1[num1 - 1] = code1[num1 - 1] + '1';
-	}
+		coding(code1, num1);
 
-	string C[256];
+	string c[256]; 
 	for (int i = 0; i < num0; i++)
-		C[i] = code0[i];
+		c[i] = code0[i];
 	for (int i = 0; i < num1; i++)
-		C[i + num0] = code1[i];
-	for (int i = 0;i < S.size();i++)
+		c[i + num0] = code1[i];
+
+	for (int i = 0;i < s.size();i++) //compressed string output
 		for (int j = 0;j < num;j++)
 		{
-			if (S[i] == MeetSym[j])
+			if (s[i] == meetsym[j])
 			{
-				cout << C[j] << ' ';
+				cout << c[j] << ' ';
 				break;
 			}
 		}
